bd6f47e9ee338f16b8c28a92eb589cbc586ac0e0
Gary Wong <Gary.T.Wong@gov.bc.ca>
2020-05-13 18:30:02 -0700

    Refactoring to Service Account Client

--- dist/kc-login.js
+++ dist/kc-login.js
@@ -1,25 +1,21 @@
 "use strict";
 Cypress.Commands.add("kcLogin", function (user) {
     Cypress.log({ name: "Login" });
-    cy.fixture("users/" + user).then(function (userData) {
-        var authBaseUrl = Cypress.env('auth_base_url');
-        var realm = Cypress.env('auth_realm');
-        var client_id = Cypress.env('auth_client_id');
-        var client_secret = Cypress.env('auth_client_secret');
-        var base64authHdr = btoa(client_id + ":" + client_secret);
-        cy.request({
-            method: 'POST',
-            url: authBaseUrl + "/realms/" + realm + "/protocol/openid-connect/token",
-            followRedirect: false,
-            form: true,
-            headers: {
-                Authorization: "Basic " + base64authHdr,
-            },
-            body: {
-                // username: userData.username,
-                // password: userData.password,
-                grant_type: 'client_credentials',
-            },
-        }).its('body');
-    });
+    var authBaseUrl = Cypress.env('auth_base_url');
+    var realm = Cypress.env('auth_realm');
+    var client_id = Cypress.env('auth_client_id');
+    var client_secret = Cypress.env('auth_client_secret');
+    var auth_header = btoa(client_id + ":" + client_secret);
+    return cy.request({
+        method: 'POST',
+        url: authBaseUrl + "/realms/" + realm + "/protocol/openid-connect/token",
+        followRedirect: false,
+        form: true,
+        headers: {
+            Authorization: "Basic " + auth_header,
+        },
+        body: {
+            grant_type: 'client_credentials',
+        },
+    }).its('body');
 });
--- src/kc-login.ts
+++ src/kc-login.ts
@@ -1,26 +1,22 @@
 Cypress.Commands.add("kcLogin", (user: string) => {
   Cypress.log({ name: "Login" });
+  const authBaseUrl = Cypress.env('auth_base_url')
+  const realm = Cypress.env('auth_realm')
+  const client_id = Cypress.env('auth_client_id')
+  const client_secret = Cypress.env('auth_client_secret')
+  const auth_header = btoa(`${client_id}:${client_secret}`)
 
-  cy.fixture(`users/${user}`).then((userData: UserData) => {
-    const authBaseUrl = Cypress.env('auth_base_url')
-    const realm = Cypress.env('auth_realm')
-    const client_id = Cypress.env('auth_client_id')
-    const client_secret = Cypress.env('auth_client_secret')
-    const base64authHdr = btoa(`${client_id}:${client_secret}`)
-
-    cy.request({
+  return cy.request({
       method: 'POST',
       url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/token`,
       followRedirect: false,
       form: true,
       headers: {
-        Authorization: `Basic ${base64authHdr}`,
+        Authorization: `Basic ${auth_header}`,
       },
       body: {
-        // username: userData.username,
-        // password: userData.password,
         grant_type: 'client_credentials',
       },
     }).its('body')
+
   });
-});
