7fe29b3d8daf53df08fcff8e0501c3121d35022c
Gary Wong <Gary.T.Wong@gov.bc.ca>
2020-05-13 20:24:03 -0700

    Refactoring to JWT Generation

--- dist/kc-login.js
+++ dist/kc-login.js
@@ -5,17 +5,24 @@ Cypress.Commands.add("kcLogin", function () {
     var realm = Cypress.env('auth_realm');
     var client_id = Cypress.env('auth_client_id');
     var client_secret = Cypress.env('auth_client_secret');
-    var auth_header = btoa(client_id + ":" + client_secret);
+    var username = Cypress.env('auth_username');
+    var password = Cypress.env('auth_password');
+    // const auth_header = btoa(`${client_id}:${client_secret}`)
     return cy.request({
         method: 'POST',
         url: authBaseUrl + "/realms/" + realm + "/protocol/openid-connect/token",
         followRedirect: false,
         form: true,
-        headers: {
-            Authorization: "Basic " + auth_header,
-        },
+        // headers: {
+        //   Authorization: `Basic ${auth_header}`,
+        // },
         body: {
-            grant_type: 'client_credentials',
-        },
+            grant_type: 'password',
+            client_id: client_id,
+            client_secret: client_secret,
+            scope: 'openid',
+            username: username,
+            password: password
+        }
     }).its('body');
 });
--- src/kc-login.ts
+++ src/kc-login.ts
@@ -4,21 +4,26 @@ Cypress.Commands.add("kcLogin", () => {
   const realm = Cypress.env('auth_realm')
   const client_id = Cypress.env('auth_client_id')
   const client_secret = Cypress.env('auth_client_secret')
-  const auth_header = btoa(`${client_id}:${client_secret}`)
+  const username = Cypress.env('auth_username')
+  const password = Cypress.env('auth_password')
+  // const auth_header = btoa(`${client_id}:${client_secret}`)
 
   return cy.request({
       method: 'POST',
       url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/token`,
       followRedirect: false,
       form: true,
-      headers: {
-        Authorization: `Basic ${auth_header}`,
-      },
+      // headers: {
+      //   Authorization: `Basic ${auth_header}`,
+      // },
       body: {
-        grant_type: 'client_credentials',
-      },
-    // }).then(response => {
-    //   return response.body;
+        grant_type: 'password',
+        client_id,
+        client_secret,
+        scope: 'openid',
+        username,
+        password
+      }
     }).its('body')
 
   });
